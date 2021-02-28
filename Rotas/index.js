50d51ac9ef883dbd720773185191be6e47ed269c
Rafael de Oliveira Domenegueti <69098216+RafaelDomenegueti@users.noreply.github.com>
2021-02-28 13:44:30 -0300

    vx

--- Rotas/index.js
+++ Rotas/index.js
@@ -1,7 +1,7 @@
 module.exports = function(app){
 
     app.get("/", function(req, res){
-        res.sendFile("C:/Users/Rafael/Documents/Projetos/Full-Stack/Paginas/views/index.html")
+        res.sendFile("C:/Users/Rafael/Documents/GitHub/Full-Stack/paginas/views/index.html")
     })
 
 }
--- server.js
+++ server.js
@@ -4,12 +4,13 @@ const app = express()
 const path = require("path")
 
 // Deixa ultilizar o css e js
-app.use(express.static(path.join(__dirname, "/Paginas")))
+app.use(express.static(path.join(__dirname, "./Paginas")))
 
 //  Rotas
 consign().include("Rotas").into(app)
 
 
-app.listen(3000, "localhost", function(){
+app.listen(8221, "192.168.15.7", function(){
     console.log("Servidor On-line")
+    console.log("On-line: 192.168.15.7:8221")
 })
