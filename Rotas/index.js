fcce8b375bc88a430c55c5ec815fbd0d1356c591
Rafael de Oliveira Domenegueti <69098216+RafaelDomenegueti@users.noreply.github.com>
2021-03-02 21:33:30 -0300

    vx

--- Rotas/index.js
+++ Rotas/index.js
@@ -1,7 +1,11 @@
-module.exports = function(app){
+module.exports = function(app, urlencodedParser){
 
     app.get("/", function(req, res){
-        res.sendFile("C:/Users/Rafael/Documents/GitHub/Full-Stack/paginas/views/index.html")
+        res.sendFile("C:/Users/Rafael/Documents/GitHub/Full-Stack/paginas/views/login.html")
     })
+    app.post("/", urlencodedParser, function(req, res){
+        var username = req.body.username
+        var pass = req.body.password
+    });
 
 }
--- server.js
+++ server.js
@@ -1,15 +1,25 @@
+// Require
 const express = require("express")
 const consign = require("consign")
 const app = express()
 const path = require("path")
+const bodyparser = require("body-parser")
 
-// Deixa ultilizar o css e js
-app.use(express.static(path.join(__dirname, "./Paginas")))
+
+// Config
+    // Deixa ultilizar o css e js
+    app.use(express.static(path.join(__dirname, "./paginas")))
+
+    // Receber informações
+    var urlencodedParser = bodyparser.urlencoded({ extended: false })
+    app.use(bodyparser.urlencoded({ extended: false }))
+
 
 //  Rotas
-consign().include("Rotas").into(app)
+consign().include("Rotas").into(app, urlencodedParser)
 
 
+// Porta
 app.listen(8221, "192.168.15.7", function(){
     console.log("Servidor On-line")
     console.log("On-line: http://192.168.15.7:8221")
